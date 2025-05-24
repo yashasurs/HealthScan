8103cd260f5fe3569c0da3ccfa05f986d9994b52
vinyas-bharadwaj <vinyasbharadwaj101@gmail.com>
2025-05-24 17:21:05 +0530

    added alembic

--- server/app/database.py
+++ server/app/database.py
@@ -1,43 +1,22 @@
-import time
-
-import psycopg2
-from psycopg2.extras import RealDictCursor
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
-
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base = declarative_base()
 
+def init_db():
+    Base.metadata.create_all(bind=engine)
 
-# Dependency
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
-
-# while True:
-#     try:
-#         conn = psycopg2.connect(
-#             host='localhost', database='fastapi', user='postgres', password='anirudh2005',
-#             cursor_factory=RealDictCursor)
-#         cursor = conn.cursor()
-#         print('Database connection was successful')
-#         break
-#     except Exception as error:
-#         print('Connecting to database failed')
-#         print('Error: ', error)
-#         time.sleep(2)
--- server/app/main.py
+++ server/app/main.py
@@ -1,17 +1,11 @@
 from fastapi import FastAPI
-from pydantic_settings import BaseSettings
-
 from . import models
-from .config import settings
 from .database import engine
-from .routers import auth, post, user
 
 models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
-
-app.include_router(auth.router)
 
 
 @app.get("/")
