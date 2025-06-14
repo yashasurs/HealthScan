db789f506a58e85849953872eb43f28ebfb302ac
dynamite-123 <anirudhkashyap182005@gmail.com>
2025-06-14 20:26:54 +0530

    fixed minor bug

--- server/app/database.py
+++ server/app/database.py
@@ -8,7 +8,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.environ.get("SUPABASE_DATABASE_URL")
 
 print(f"Connecting to database at {SQLALCHEMY_DATABASE_URL}")
 
